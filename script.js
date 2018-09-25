3b1610448ea39d697538f66b098d03fc25da8a20
NunoClodic Carvalho Lima <32763809+NunoCLodic@users.noreply.github.com>
2018-09-25 01:49:26 -0300

    Versao 1
    Versao 1 do Projeto de Tec Web

---
+++ script.js
@@ -0,0 +1,15 @@
+function calculaIMC() {
+	var peso = document.getElementById("peso").innerHTML;
+	var altura = document.getElementById("altura").innerHTML;
+	var imc = peso * altura * altura;
+	addPaciente(imc)
+}
+
+function excluir() {
+
+}
+
+function addPaciente(imc) {
+
+}
+
