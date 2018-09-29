11fe91484ce9b0f5d79a4ad27e71767996bc6134
NunoClodic Carvalho Lima <32763809+NunoCLodic@users.noreply.github.com>
2018-09-29 12:42:57 -0300

    Versao 7
    Versao 7 do Projeto

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+/*Botao voltar ao Topo da Pagina*/
+$(document).ready(function() {
+	$('#subir').click(function(){
+		$('html, body').animate({scrollTop:0}, 'slow');
+		return false;
+	});
+});
+
+/*Função para calcular o IMC*/
 function calculaIMC() {
 	var peso = document.getElementById("peso").innerHTML;
 	var altura = document.getElementById("altura").innerHTML;
